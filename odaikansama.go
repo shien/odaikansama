8199e6c30327da446b85b57b0b2104a68f10fb06
Taiki TAIRA <sienrizmu@gmail.com>
2018-03-20 21:54:40 +0900

    add new theme and keep alive message.

--- odaikansama.go
+++ odaikansama.go
@@ -9,7 +9,10 @@ import (
 )
 
 const (
-	requestMessage = "お題くれ"
+	requestMessage         = "お題くれ"
+	requestMessageClothing = "服のお題くれ"
+	keepAlive              = "生きてる？"
+	answerAlive            = "はい！！！元気です！！！"
 )
 
 func run(api *slack.Client) int {
@@ -28,6 +31,11 @@ func run(api *slack.Client) int {
 				if ev.Text == requestMessage {
 					theme := ChoiceTheme()
 					rtm.SendMessage(rtm.NewOutgoingMessage(theme, ev.Channel))
+				} else if ev.Text == requestMessageClothing {
+					theme := ChoiceClothing()
+					rtm.SendMessage(rtm.NewOutgoingMessage(theme, ev.Channel))
+				} else if ev.Text == keepAlive {
+					rtm.SendMessage(rtm.NewOutgoingMessage(answerAlive, ev.Channel))
 				}
 
 			case *slack.InvalidAuthEvent:
@@ -37,6 +45,103 @@ func run(api *slack.Client) int {
 			}
 		}
 	}
+}
+
+func ChoiceClothing() string {
+
+	clothing_bottoms := []string{
+		"ミニスカート",
+		"フレアスカート",
+		"ギャザースカート",
+		"プリーツスカート",
+		"ペンシルスカート",
+		"コクーンスカート",
+		"ペンシルスカート",
+		"ペンシルスカート",
+		"プリーツスカート",
+		"タイトスカート",
+		"カプリパンツ",
+		"サルエルパンツ",
+		"スラックス",
+		"スカンツ",
+		"デニム",
+		"リジットデニム",
+		"ベイカーパンツ",
+		"ストレートデニム",
+		"ボーイフレンド",
+		"スキニー",
+		"バギーパンツ",
+		"テーパードパンツ",
+		"パラッツォパンツ",
+		"ジョガーパンツ",
+		"チノ・パン"}
+	clothing_bottom := clothing_bottoms[rand.Intn(len(clothing_bottoms))]
+
+	clothing_tops := []string{
+		"ワイシャツ",
+		"作業着",
+		"法被",
+		"セーラー",
+		"ワンピース",
+		"ニットシャツ",
+		"ブラウス",
+		"ポロシャツ",
+		"デニムシャツ",
+		"ニットシャツ",
+		"Tシャツ",
+		"カットソー",
+		"チュニック",
+		"タンクトップ",
+		"キャミソール",
+		"ベアトップ",
+		"ニット",
+		"ベスト",
+		"カーディガン",
+		"スウェット",
+		"パーカー",
+		"ジャージ"}
+	clothing_top1 := clothing_tops[rand.Intn(len(clothing_tops))]
+	clothing_top2 := clothing_tops[rand.Intn(len(clothing_tops))]
+
+	shoebox := []string{
+		"ブーツ",
+		"スニーカー",
+		"サンダル",
+		"パンプス",
+		"ショートブーツ",
+		"ニーハイブーツ",
+		"ハイヒール",
+		"厚底靴",
+		"下駄",
+		"はだし",
+		"草履",
+		"島草履",
+		"雨靴",
+		"ローラースケート",
+		"ローラーブレード",
+		"アイススケート",
+		"足袋"}
+	shoes := shoebox[rand.Intn(len(shoebox))]
+
+	option := ChoiceOption()
+
+	theme := clothing_top1 + "か" + clothing_top2 + "か、もしくは両方を合わせた服に" + clothing_bottom + "を描きましょう。履物は" + shoes + "で、" + option + "もおまけでいかがでしょうか。"
+
+	return theme
+
+}
+
+func ChoiceOption() string {
+	options := []string{
+		"小動物",
+		"鞄",
+		"眼鏡",
+		"帽子",
+		"パンスト",
+		"色黒",
+		"手袋",
+		"眼帯"}
+	return options[rand.Intn(len(options))]
 }
 
 func ChoiceTheme() string {
@@ -57,14 +162,13 @@ func ChoiceTheme() string {
 	hairStyles := []string{"長髪", "ポニーテール", "ツインテール", "短髪", "サイドテール", "ぼさぼさ"}
 	hairStyle := hairStyles[rand.Intn(len(hairStyles))]
 
-	clothing_list := []string{"警察官の服", "メイド服／執事服", "ドレス／タキシード", "体操服", "巫女服／神主", "水着", "学生服", "看護士／医者", "私服"}
+	clothing_list := []string{"警察官の服", "メイド服／執事服", "ドレス／タキシード", "体操服", "巫女服／神主服", "水着", "学生服", "看護服／白衣", "私服", "道着"}
 	clothing := clothing_list[rand.Intn(len(clothing_list))]
 
 	sex_list := []string{"男", "女"}
 	sex := sex_list[rand.Intn(len(sex_list))]
 
-	options := []string{"小動物", "眼鏡", "帽子", "パンスト", "ブーツ", "色黒", "手袋", "眼帯"}
-	option := options[rand.Intn(len(options))]
+	option := ChoiceOption()
 
 	theme := expression + hairStyle + "の" + clothing + "を着た" + sex + "を描きましょう。おまけで" + option + "もいれてみては。"
 
@@ -72,6 +176,6 @@ func ChoiceTheme() string {
 }
 
 func main() {
-	api := slack.New("APIKEY")
+	api := slack.New("APPKEY")
 	os.Exit(run(api))
 }
