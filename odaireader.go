abd4609323c67af44c4046eca9e7a0a86041bdb7
Taiki TAIRA <sienrizmu@gmail.com>
2018-05-03 01:13:50 +0900

    add odai read from csv file and cache

--- odaireader.go
+++ odaireader.go
@@ -7,14 +7,37 @@ import (
 	"os"
 )
 
-const (
-	clothing = "服"
-	theme    = "テーマ"
-)
+type OdaiCache struct {
+	Data []Odai
+}
 
-func GetOdaiList(odai_type string, odai_subtype string) []string {
+type Odai struct {
+	OdaiType    string
+	OdaiSubtype string
+	OdaiList    []string
+}
 
-	fp, err := os.OpenFile("odai.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
+func (cache *OdaiCache) GetOdai(OdaiType string, OdaiSubtype string) Odai {
+
+	length := len(cache.Data)
+	for i := 0; i < length; i++ {
+		if cache.Data[i].OdaiType == OdaiType && cache.Data[i].OdaiSubtype == OdaiSubtype {
+			return cache.Data[i]
+		}
+	}
+
+	var odai Odai
+	odai.OdaiList = ReadFile(OdaiType, OdaiSubtype)
+	odai.OdaiType = OdaiType
+	odai.OdaiSubtype = OdaiSubtype
+
+	cache.Data = append(cache.Data, odai)
+	return odai
+}
+
+func ReadFile(OdaiType string, OdaiSubtype string) []string {
+
+	fp, err := os.OpenFile("odai.csv", os.O_RDONLY|os.O_CREATE, os.ModePerm)
 	if err != nil {
 		panic(err)
 	}
@@ -33,13 +56,9 @@ func GetOdaiList(odai_type string, odai_subtype string) []string {
 		if err != nil {
 			log.Fatal(read_err)
 		}
-		if (record[0] == odai_type) && (record[1] == odai_subtype) {
+		if (record[0] == OdaiType) && (record[1] == OdaiSubtype) {
 			result = append(result, record[2])
 		}
 	}
 	return result
 }
-
-//func main() {
-//	fmt.Println(GetOdaiList("服", "靴"))
-//}
